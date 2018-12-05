65081e8805c296c38e0f08b369756d86f3db6d82
ajbpasigado <ajpasigado@outlook.com>
2018-12-05 13:57:08 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/ajbpasigado/pasigado_activity4/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ajbpasigado.pasigado_activity4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View v){
+        Intent intent = new Intent(this, GameStart.class);
+        startActivity(intent);
+    }
+
+    public void quit(View v){
+        finish();
+    }
+
+
+}
